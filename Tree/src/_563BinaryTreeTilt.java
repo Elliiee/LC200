4228ac049e6123a748b00b2ca85be7da0c1a6643
Elliiee <danyiluozhang@gmail.com>
2020-04-11 20:35:06 -0500

    563

---
+++ Tree/src/_563BinaryTreeTilt.java
@@ -0,0 +1,15 @@
+public class _563BinaryTreeTilt {
+    int tilt = 0;
+    public int findTilt(TreeNode root) {
+        dfs(root);
+        return tilt;
+    }
+
+    public int dfs(TreeNode root){
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        tilt += Math.abs(left - right);
+        return left + right + root.val;
+    }
+}
